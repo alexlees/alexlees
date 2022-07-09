c7b20be7688a50ebd898766a7fdf72ca3573270d
alexlees <18323965465@163.com>
2022-07-09 13:21:12 +0000

    fix: blog

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 import blog, { ga, redirects } from "https://deno.land/x/blog@0.3.3/blog.tsx";
 
 blog({
-  title: "alexlees",
-  author: "alexlees",
-  description: "alexlees's blog",
-  avatarClass: "rounded-full",
-  avatar: 'https://avatars.githubusercontent.com/u/38124537',
-  links: [
-    { title: "GitHub", url: "https://github.com/alexlees" },
-  ],
-  background: "#f9f9f9",
-  lang: "zh-cn",
-  timezone: "zh-cn",
-  middlewares: [
-    ga("G-9RKQCCX1SR"),
-    redirects({}),
-  ],
+  // title: "alexlees",
+  // author: "alexlees",
+  // description: "alexlees's blog",
+  // avatarClass: "rounded-full",
+  // avatar: 'https://avatars.githubusercontent.com/u/38124537',
+  // links: [
+  //   { title: "GitHub", url: "https://github.com/alexlees" },
+  // ],
+  // background: "#f9f9f9",
+  // lang: "zh-cn",
+  // timezone: "zh-cn",
+  // middlewares: [
+  //   ga("G-9RKQCCX1SR"),
+  //   redirects({}),
+  // ],
 });
